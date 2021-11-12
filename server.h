4a5e92f854e68e54587357b76f4023da51ea88ba
yasminewatkins <yasminepwatkins@gmail.com>
2021-11-12 15:57:48 -0500

    Added queue functions

--- server.h
+++ server.h
@@ -1,36 +1,37 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <memory.h>
-#include <pthread.h>
+#include <arpa/inet.h>
 
+#define DEFAULT_DICTIONARY "words.txt"
 #define NUM_WORK 10
 
+typedef struct Node {
+  struct sockaddr_in client_addr;
+  int client_socket;
+  char *word;
+  struct Node *next;
+}Node;
+
+typedef struct Queue {
+  Node *head;
+  int qsize;
+}Queue;
+
+Queue *initQueue();
+Node *initNode(struct sockaddr_in, char *, int);
+void addQueue(Queue *, struct sockaddr_in , char *, int);
+void *removeQueue(Queue *);
+int empty(Queue *);
 void *worker_thread(void *);
 void *log_thread(void *);
 
-typedef struct node {
-    void *data;
-    struct node *next;
-} node;
-
-typedef struct queue {
-    node *head;
-    node *tail;
-    int len;
-} queue;
-
-queue *initQueue();
-node *initNode(void *);
-void addQueue(queue *, void *);
-void *removeQueue(queue *);
-int empty(queue *);
-
-#endif 
+#endif //SERVER_H
