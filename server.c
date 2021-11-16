963fb130763becfade4b2049245f338e320172cc
yasminewatkins <yasminepwatkins@gmail.com>
2021-11-16 14:17:41 -0500

    Added put and get functions

--- server.c
+++ server.c
@@ -1,11 +1,11 @@
 #include "server.h"
 
-Queue *workQueue; 
-Queue *logQueue; 
-pthread_mutex_t work_lock, log_lock, logQueue_lock;
-pthread_cond_t cond1, cond2, cond3, cond4; 
+// Global buffer variables.
+socketBuff socket_buff;
+logBuff log_buff;
 
-char** dict_words; //Holds all words found in dictionary
+int sizeMax = 5; 
+char **dictWords; 
 
 char ** dictionary(char *filename) {
 
@@ -21,7 +21,7 @@ char ** dictionary(char *filename) {
     if(output == NULL) {
         printf("Memory allocation error.\n");
         exit(1);
-     }
+    }
 
     char line [bufferSize];
     int index = 0; 
@@ -33,46 +33,65 @@ char ** dictionary(char *filename) {
         if(output[index] == NULL) {
             printf("Memory allocation error.\n");
             exit(1);
+        }
+
+        int temp = strlen(line) - 2;
+        line[temp] = '\0';
+        strcpy(output[index], line);
+        index++;
     }
 
-    int temp = strlen(line) - 2;
-    line[temp] = '\0';
-    strcpy(output[index], line);
-    index++;
-  }
+    fclose(file);
+    return output;
 
-  fclose(file);
-  return output;
+}
 
-  }
+void put(int client_socket) { 
+    //This locks to work queue
+    pthread_mutex_lock(&(socket_buff.work_lock));
+    while(socket_buff.array_count >= sizeMax) {
+        pthread_cond_wait(&socket_buff.cond_empty, &socket_buff.work_lock);
+    }
+        
+    //Gets the first job off the queue 
+    socket_buff.array[socket_buff.fill_ptr] = client_socket; 
+    socket_buff.fill_ptr = (socket_buff.fill_ptr + 1) % socket_buff.array_count; 
+    socket_buff.array_count++; 
+    pthread_cond_signal(&socket_buff.cond_full);
+    pthread_mutex_unlock(&socket_buff.work_lock);
 
-void *worker_thread(void *arg) {
+}
+
+int get() { 
+    //This locks to work queue
+    pthread_mutex_lock(&(socket_buff.work_lock));
+    while(socket_buff.array_count < 1) {
+        pthread_cond_wait(&socket_buff.cond_full, &socket_buff.work_lock);
+    }
+    //Gets the first job off the queue 
+    int client = socket_buff.array[socket_buff.use_ptr]; 
+    socket_buff.use_ptr = (socket_buff.use_ptr + 1) % socket_buff.array_count; 
+    socket_buff.array_count--; 
+    pthread_cond_signal(&socket_buff.cond_empty);
+    pthread_mutex_unlock(&socket_buff.work_lock);
+    return client;
+
+}
+
+void* worker_thread(void* args) {
     char *promptMsg = "Enter word to spellcheck: ";
     char *closeMsg = "Connection to server is terminated!\n";
     char *errorMsg = "Error displaying message!\n";
 
-    while (1) {
-        //This locks to work queue
-        pthread_mutex_lock(&work_lock);
-
-        if (workQueue->qsize <= 0) {
-            pthread_cond_wait(&cond1, &work_lock);
-    }
-
-    //Poping the first job off the queue 
-    Node *job = pop(workQueue);
-    pthread_mutex_unlock(&work_lock);
-    pthread_cond_signal(&cond2);
-
-    int client_socket = job->client_socket;
+    int client_socket = get();
 
     while (1) { 
-        char buff_recv[bufferSize] = " ";
+        char buff_recv[bufferSize] = "";
         //This sends the prompt to the buffer
         send(client_socket, promptMsg, strlen(promptMsg), 0);
-        int returnedWord = recv(client_socket, buff_recv, BUF_SIZE, 0);
+        int returnedWord = recv(client_socket, buff_recv, bufferSize, 0);
         //Error check
-        if (returnedWord <= -1) {
+        if (returnedWord < 0) {
             send(client_socket, errorMsg, strlen(errorMsg), 0);
             continue;
         //Quitting client
@@ -84,12 +103,13 @@ void *worker_thread(void *arg) {
         } else {
             buff_recv[strlen(buff_recv) - 1] = '\0';
             buff_recv[returnedWord - 2] = '\0';
+        
         //Word is mispelled
-        char *result = " INCORRECT\n";
+        char *result = " MISSPELLED\n";
         for (int i = 0; i < DICTIONARY_LENGTH; i++) {
             //Word is spelled correctly
-            if (strcmp(buff_recv, dict_words[i]) == 0) {
-                result = " CORRECT\n";
+            if (strcmp(buff_recv, dictWords[i]) == 0) {
+                result = " OK\n";
                 break;
             }
         }
@@ -98,81 +118,97 @@ void *worker_thread(void *arg) {
         printf("%s", buff_recv);
         send(client_socket, buff_recv, strlen(buff_recv), 0);
 
-        struct sockaddr_in client = job->client_addr;
+        // //Locking log 
+        // pthread_mutex_lock(&logQueue_lock);
 
-        //Locking log 
-        pthread_mutex_lock(&log_lock);
+        // if(logQueue->qsize >= sizeMax) {
+        //     pthread_cond_wait(&empty, &logQueue_lock);
 
-        if(logQueue->qsize >= sizeMax) {
-            pthread_cond_wait(&cond4, &logQueue_lock);
+        // }
+        // // Adds result to log buffer
+        // push(logQueue, client, buff_recv, client_socket);
+        // pthread_mutex_unlock(&logQueue_lock);
+        // pthread_cond_signal(&full);
 
         }
-        // Adds result to log buffer
-        push(logQueue, client, buff_recv, client_socket);
-        pthread_mutex_unlock(&logQueue_lock);
-        pthread_cond_signal(&cond3);
-
-      }
     }
-  }
-}
+    return 0; 
+} 
 
+// void *log_thread(void *arg) {
 
-void *log_thread(void *arg) {
-    FILE *log = fopen("log.txt", "r");
+//      while(1) {
+//         /* Locks the log queue. */
+//         pthread_mutex_lock(&logQueue_lock);
+//         if (logQueue->qsize == 0) {
+//             /* Waits if empty. */
+//             pthread_cond_wait(&full, &logQueue_lock);
+//         }
+//         /* Gets the word. */
+//         Node *node = pop(logQueue);
+//         char *word = node->word;
 
-    while (1) {
-        pthread_mutex_lock(&log_lock);
-        while (empty(logQueue))
-            pthread_cond_wait(&cond2, &log_lock);
-    
-        char *msg = removeQueue(logQueue);
-        pthread_mutex_unlock(&log_lock);
-        fprintf(log, "%s", msg);
-        fflush(log);
-        free(msg);
-    }
+//         /* Releases the lock. */
+//         pthread_mutex_unlock(&logQueue_lock);
+//         /* Sends the signal. */
+//         pthread_cond_signal(&empty);
 
-}
+//         /* If empty do nothing. */
+//         if (word == NULL) {
+//             continue;
+//         }
 
-int main(int argc, char *argv[]) {
-    int port = 8889; //Default port  
+//         /* Lock the log file. */
+//         pthread_mutex_lock(&log_lock);
+
+//         /* Write results to log file. */
+//         FILE *log_file = fopen(DEFAULT_LOG_FILE, "a");
+//         fprintf(log_file, "%s", word);
+//         fclose(log_file);
+
+//         /* Releases the lock. */
+//         pthread_mutex_unlock(&log_lock);
+//     }
+// }
+
+int main(int argc, char **argv) { 
     char *dict; 
-    dict_words = dictionary(dict); //Gets the words found in the dictionary 
+    int port; 
 
-    pthread_mutex_init(&work_lock, NULL);
-    pthread_mutex_init(&log_lock, NULL);
-    pthread_mutex_init(&logQueue_lock, NULL);
-    pthread_cond_init(&cond1, NULL);
-    pthread_cond_init(&cond2, NULL);
-    pthread_cond_init(&cond3, NULL);
-    pthread_cond_init(&cond4, NULL);
+    //Determines which port and dictionary to use 
+    if(argc == 1){ 
+        port = DEFAULT_PORT; 
+        dict = DEFAULT_DICTIONARY; 
 
-     // Delcare thread pool for workers and thread for logging
+    } else if(argc == 2){ 
+        port = atoi(argv[1]); 
+        dict = DEFAULT_DICTIONARY; 
+    } else { 
+        port = atoi(argv[1]); 
+        dict = argv[2]; 
+
+    }
+   
+    pthread_mutex_init(&socket_buff.work_lock, NULL);
+    pthread_mutex_init(&log_buff.log_lock, NULL);
+    pthread_cond_init(&socket_buff.cond_full, NULL);
+    pthread_cond_init(&socket_buff.cond_empty, NULL);
+    //pthread_cond_init(log_buff. , NULL);
+    //pthread_cond_init(log_buff.cond_empty, NULL);
+
+    // Delcare thread pool for workers and thread for logging
     pthread_t workers[NUM_WORK];
+    
     for (int i = 0; i < NUM_WORK; i++) {
         pthread_create(&workers[i], NULL, &worker_thread, NULL);
     }
-    pthread_t logger;
-    pthread_create(&logger, NULL, &log_thread, NULL);
-    
-    //Determines which port and dictionary to use 
-    if(argc == 1) {
-        port;  
-        dict = DEFAULT_DICTIONARY;
-    } else if(argc == 2) {
-        port = atoi(argv[1]);
-        dict = DEFAULT_DICTIONARY;
-    } else {
-        port = atoi(argv[1]);
-        dict = argv[2];
-    }
+  
+    //pthread_t logger;
+    //pthread_create(&logger, NULL, &log_thread, NULL);
 
      //Socket creation and declation of variables 
     int socketfd; 
     char *message = "You are now connected to server. Please enter -1 to exit.\n";
-    char *fullBuff = "The job buffer is full!\n";
-
     socketfd = socket(AF_INET, SOCK_STREAM, 0); 
 
     if(socketfd == -1){ 
@@ -204,22 +240,11 @@ int main(int argc, char *argv[]) {
         if(result == -1) { 
             puts("Error sending message!"); 
         }
-
-        // Checks if full and locks work queue 
-        pthread_mutex_lock(&work_lock);
-
-        if(workQueue->qsize >= sizeMax) {
-            send(client_socket, fullBuff, strlen(fullBuff), 0);
-            pthread_cond_wait(&cond2, &work_lock);
-        }
-
-        push(workQueue, address, NULL, client_socket);
-        pthread_mutex_unlock(&work_lock);
-        pthread_cond_signal(&cond1);
+        
+        put(client_socket);
     
     }  
 
     close(socketfd);
     return 0; 
-} 
-
+}
